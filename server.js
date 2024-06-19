52afe8a5b8186907bf7bff6f0cdceb345b5cd8b3
Ibrahim SOMA <ibrahim.soma@hetic.net>
2024-06-19 13:24:40 +0200

    learned about routes in express

--- server.js
+++ server.js
@@ -1,105 +1,31 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 const path = require('path')
-const fs = require('fs')
-const fsPromisses = require('fs').promises
-const logEvents =  require('./logEvents')
-const EventEmmiter = require('events')
-class MyEmmiter extends EventEmmiter {}
-
-const myEmitter = new MyEmmiter()
-
-myEmitter.on('log', (msg , fileName) => logEvents(msg, fileName))
- 
 const PORT = process.env.PORT || 3500
 
-const servFile =  async (filePath, contentType, response) =>  {
-    try {
-        const rawData = await fs.promises.readFile(filePath, 
-            !contentType.includes('image') ? "utf8" : "")
-        const data = contentType === 'Application/json' ? JSON.parse(rawData) : rawData
-        response.writeHead(filePath.includes('404.html') ? 404 : 200, {'Content-Type': contentType})
-        response.end(contentType = 'application/json' ? JSON.stringify(data)  : data)
-    } catch (err) {
-        console.log(err);
-        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt')
-        response.statusCode = 500
-        response.end()
-    }
-}
+app.get('^/$|/index(.html)?', (req, res) => {
+    // res.sendFile('./views/index.html', {root : __dirname }) or
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
+} ) 
 
-    const extension = path.extname(req.url)
-    let contentType
+app.get('/new-page(.html)?', (req, res) => {
+    // res.sendFile('./views/index.html', {root : __dirname }) or
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+}) 
 
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-        default:
-            contentType = 'text/html';
-    }
-
-    let filePath =
-    contentType === 'text/html' && req.url === '/'
-        ? path.join(__dirname, 'views', 'index.html')
-        : contentType === 'text/html' && req.url.slice(-1) === '/'
-            ? path.join(__dirname, 'views', req.url, 'index.html')
-            : contentType === 'text/html'
-                ? path.join(__dirname, 'views', req.url)
-                : path.join(__dirname, req.url);
-    
-    // make the .tml extention not required in the broswer
-    if (!extension && req.url.slice(-1) !== '/') filePath += '.html'
-
-    const fileExists = fs.existsSync(filePath)
-
-    if (fileExists){
-        servFile(filePath,contentType, res)
-    }else {
-        //404
-        //301
-        switch(path.parse(filePath).base){
-            case 'ok.html':
-                res.writeHead(404, {'Location': '/new-page.html'})
-                res.end()
-                break
-            case 'www-page.html':
-                res.writeHead(301, {'Location': '/'})
-                res.end()
-                break
-            default: 
-            // ser a 404 response
-                servFile(path.join(__dirname, 'views', '404.html'),'text/html', res)
-
-        }
-    }
-
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html') // 302 bu default
+}) 
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempt to load hello.html');
+    next()
+},(req, res) => {
+      res.send('hello >World')  
 })
 
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+}) 
 
-server.listen(PORT, () => console.log('server runnin on port ' + PORT))
-
-
-
-// add listener for the log event
-
-//Emit Event
-// myEmitter.emit('log', 'log event emitted')
+app.listen(PORT, () => console.log('server runnin on port ' + PORT))
